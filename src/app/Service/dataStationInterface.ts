25718d1f6a51ed735bb5666ddf25ded36ad1a60f
Josias <yahsejosias@gmail.com>
2021-04-30 23:40:40 +0200

    finalisation du projet

---
+++ src/app/Service/dataStationInterface.ts
@@ -0,0 +1,10 @@
+export interface IsataSatations{
+  records:[
+    fields:{
+      commune : string,
+      etat : string,
+      nbvelosdispo : number,
+      nbplacesdispo : number,
+    }];
+  nom : string;
+}
--- src/app/Service/vlille-data.service.ts
+++ src/app/Service/vlille-data.service.ts
@@ -1,59 +1,80 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Istations } from './sataionsInterface';
+import { map } from 'rxjs/operators';
+import { IsataSatations } from './dataStationInterface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class VLilleDataService {
-
-
   //private _url: string = "/assets/data/dataTest.json";
-  private  _urlDataJson: string = "https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion&refine.nom=THEATRE+SEBASTOPOL";
+  private _urlDataJson: string =
+    'https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion&refine.nom=';
+  private _urlAllStationJson: string =
+    'https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&rows=252&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion';
 
-  public  station ={
-        commune :"",
-        station : "",
-        service : "",
-        veloDispo: 0,
-        placeDispo :0 }
+  public saveStations = [] as any;
+  public savestationSerach = [] as any;
 
-  public  saveStations =[{
-        "commune" :"Lille",
-        "station" : "Hôpitale",
-        "service" : "HORS SERVICE",
-        "veloDispo": 2,
-        "placeDispo" : 15
-      },
-      {
-        "commune" :"Lille",
-        "station" : "Croix",
-        "service" : "EN SERVICE",
-        "veloDispo": 0,
-        "placeDispo" : 3
-      }] as any;
+  constructor(private http: HttpClient) {}
 
+  getDataSearch(station: string): Observable<IsataSatations[]> {
+    return this.http.get<IsataSatations[]>(station).pipe(
+      map((stations: any) => {
+        if (stations['records'].length === 0) {
+          alert('Station introuvable');
+        } else {
+          let laStation = {
+            commune: stations['records'][0]['fields']['commune'],
+            station: stations['records'][0]['fields']['nom'],
+            service: stations['records'][0]['fields']['etat'],
+            veloDispo: stations['records'][0]['fields']['nbvelosdispo'],
+            placeDispo: stations['records'][0]['fields']['nbplacesdispo'],
+          };
 
-  constructor(private http: HttpClient) { }
-
-   searchStation(){
-
-    let data = JSON.parse(this._urlDataJson);
-
-    this.station.commune= data['records'][0]['fields']['commune'];
-    this.station.commune= data['records'][0]['nom'];
-    this.station.commune= data['records'][0]['fields']['etat'];
-    this.station.commune= data['records'][0]['fields']['nbvelosdispo'];
-    this.station.commune= data['records'][0]['fields']['nbplacesdispo'];
-
-    let stationJson = JSON.stringify(this.station);
-
-    this.saveStations.push(stationJson)
-
+          this.savestationSerach.push(laStation);
+        }
+        return stations;
+      })
+    );
   }
 
-    getStation() {
-    return this.saveStations;
+  getData(): Observable<IsataSatations[]> {
+    return this.http.get<IsataSatations[]>(this._urlAllStationJson).pipe(
+      map((stations: any) => {
+        for (let pos = 0; pos < stations['nhits']; pos++) {
+          let laStation = {
+            commune: stations['records'][pos]['fields']['commune'],
+            station: stations['records'][pos]['fields']['nom'],
+            service: stations['records'][pos]['fields']['etat'],
+            veloDispo: stations['records'][pos]['fields']['nbvelosdispo'],
+            placeDispo: stations['records'][pos]['fields']['nbplacesdispo'],
+          };
+          this.saveStations.push(laStation);
+        }
+        return stations;
+      })
+    );
+  }
+
+  searchStation(station: string) {
+    let linksatationJson = this._urlDataJson + station;
+    this.getDataSearch(linksatationJson).subscribe();
+  }
+
+  getStation() {
+    if (this.savestationSerach.length == 0) {
+      this.getData().subscribe();
+      return this.saveStations;
+    } else {
+      return this.savestationSerach;
+    }
+  }
+
+  clear() {
+    let newSavestationSerach = [] as any;
+    this.savestationSerach = newSavestationSerach;
+    this.getData();
   }
 }
